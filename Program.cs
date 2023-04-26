2d3a81e8eee8efb08060c52f3e9e2018bcea95f7
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-04-26 09:51:45 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Determine se um ano fornecido pelo usuário é ou não bissexto.
+Console.WriteLine("--- Anos Bissexto ---\n");
+
+Console.Write("Digite o ano...:");
+int ano = int.Parse(Console.ReadLine()!);
+
+if ((ano % 400 == 0) || ((ano % 4 == 0) && (ano % 100 != 0))) {
+    Console.WriteLine($"\nO ano {ano} é bissexto.", ano);
+}
+else {
+    Console.WriteLine($"\nO ano {ano} não é bissexto.", ano);
+}
